3643fe5f13882744e64845769291d8f09ce60e07
StephenLujan <sLujan@catalystitservices.com>
2012-10-23 13:44:07 -0400

    fixed YET ANOTHER BAD MERGE

--- client/web/multiple_choice.dart
+++ client/web/multiple_choice.dart
@@ -1,103 +1,4 @@
-<<<<<<< HEAD
 part of test;
-//part 'question.dart';
-//part 'answer.dart';
-
-/**
- * A multiple choice question
- */
-class MultipleChoice extends Question{
-  
-  /**
-   * the selectable answers for this question
-   */
-  List<Answer> answers = new List<Answer>();
-  
-  /**
-   * the answer selected by the test taker
-   */
-  Answer selected;
-  
-  /**
-   * Constructor
-   */
-  MultipleChoice(text){
-    this.text = text;
-  }
-  
-  /**
-   * Get the greatest number of points it is possible to receive on this question
-   */
-  int getMaximumPoints() {
-    var highest = 0;
-    for (var iterator in this.answers) {
-      if (iterator.getPoints() > highest)
-        highest = iterator.getPoints();
-    }
-    return highest;
-  }
-  
-  /**
-   * Get the number of points the user earns for their answer to the question.
-   */
-  int getSelectedPoints() {
-    if (selected == null)
-      return 0;
-    return selected.points;
-  }
-  
-  /**
-   * Get the question displayed in html.
-   */
-  String display()
-  {
-    var output = new StringBuffer();
-    output.add("<p>");
-    output.add(this.text);
-    output.add("</p>");
-    for (var iterator in this.answers)
-    {
-      output.add("<input type=\"radio\" name=\"group1\" onClick=\"test()\"");
-      output.add(iterator.text);
-      output.add("\">");
-      output.add(iterator.text);
-      output.add("<br/>");
-    }
-    output.add("<br/>");
-    output.add("<input type=\"button\" value=\"Submit\">");
-    return output.toString();
-  }
-  
-  
-  bool gotMaxPoints() 
-  {
-   return selected.points == this.getMaximumPoints(); 
-  }
-    
-  String explain() {
-    var output = new StringBuffer();
-    output.add("You selected: ");
-    output.add(selected.text);
-    output.add("<br/>");
-    output.add(selected.explanation);
-    for (var iterator in this.answers)
-    {
-      if (iterator.points == this.getMaximumPoints()) 
-      {
-        output.add("<br/>");
-        output.add("A maximally agile answer is: ");
-        output.add(iterator.text);
-        output.add("<br/>");
-        output.add(iterator.explanation);
-      }
-    }
-  }
-  
-}
-=======
-part of test;
-//part 'question.dart';
-//part 'answer.dart';
 
 /**
  * A multiple choice question
@@ -192,7 +93,7 @@ class MultipleChoice extends Question
   {
     return selected.points == this.getMaximumPoints(); 
   }
-    
+  
   String explain() {
     var output = new StringBuffer();
     output.add("You selected: ${selected.text}");
@@ -210,4 +111,3 @@ class MultipleChoice extends Question
     return output.toString();
   }
 }
->>>>>>> 4c462abff19aa3d8518a99e83af8d120230529e3
