7412ab4518e3c2bb8c6d59437e347e2b1c9554c2
BobHunsicker <rhunsicker@catalystitservices.com>
2012-10-23 11:48:02 -0400

    Updates

--- client/web/multiple_choice.dart
+++ client/web/multiple_choice.dart
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 part of test;
 //part 'question.dart';
 //part 'answer.dart';
@@ -93,3 +94,132 @@ class MultipleChoice extends Question{
   }
   
 }
+=======
+part of test;
+//part 'question.dart';
+//part 'answer.dart';
+
+/**
+ * A multiple choice question
+ */
+class MultipleChoice extends Question
+{
+  
+  /**
+   * the selectable answers for this question
+   */
+  List<Answer> answers = new List<Answer>();
+  
+  /**
+   * the answer selected by the test taker
+   */
+  Answer selected;
+  
+  /**
+   * Constructor
+   */
+  MultipleChoice(text)
+  {
+    this.text = text;
+  }
+  
+  /**
+   * Get the greatest number of points it is possible to receive on this question
+   */
+  int getMaximumPoints()
+  {
+    var highest = 0;
+    for (var iterator in this.answers)
+    {
+      if (iterator.points > highest)
+        highest = iterator.points;
+    }
+    return highest;
+  }
+  
+  /**
+   * Get the number of points the user earns for their answer to the question.
+   */
+  int getSelectedPoints()
+  {
+    if (selected == null)
+      return 0;
+    return selected.points;
+  }
+  
+  /**
+   * Sets the selected answer to a given index in the answers arraylist
+   */
+  void setAnswer(int number)
+  {
+    print("setting answer to $number");
+    this.selected = this.answers[number];
+    print("answer: ${this.selected.text}");
+  }
+  
+  /**
+   * Return a DOM element radio button with an onclick event for this answer
+   */
+  Element _makeRadioButton(Answer answer, int number)
+  {
+    var sb = new StringBuffer();
+    sb.add("<input type=\"radio\" name=\"group1\">");
+    sb.add(number);
+    sb.add(");\">");
+    /*
+    var button = new ButtonElement();
+    button.innerHTML = sb.toString();
+    */
+    var element = new Element.html("<input type=\"radio\" name=\"group1\">");
+    element.on.click.add(
+        (event) => this.setAnswer(number));
+    return element;
+  }
+  
+  /**
+   * Build the question into a DOM element.
+   */
+  Element display()
+  {
+    var output = new DivElement();
+    output.insertAdjacentElement('beforeEnd',new Element.html("""<p>${this.text}</p>"""));
+    var number = 0;
+    for (var iterator in this.answers)
+    {
+      var button = this._makeRadioButton(iterator, number);
+      number++;
+      output.insertAdjacentElement('beforeEnd', button);
+      output.addText(iterator.text);
+      output.addHTML("<br/>");
+    }
+    output.addHTML("<input id=\"submit\" type=\"button\" value=\"Click Me.\">");
+    return output;
+  }
+  
+  
+  bool gotMaxPoints() 
+  {
+    return selected.points == this.getMaximumPoints(); 
+  }
+    
+  String explain() {
+    var output = new StringBuffer();
+    output.add("You selected: ");
+    output.add(selected.text);
+    output.add("<br/>");
+    output.add(selected.explanation);
+    for (var iterator in this.answers)
+    {
+      if (iterator.points == this.getMaximumPoints()) 
+      {
+        output.add("<br/>");
+        output.add("A maximally agile answer is: ");
+        output.add(iterator.text);
+        output.add("<br/>");
+        output.add(iterator.explanation);
+      }
+    }
+    return output.toString();
+  }
+}
+>>>>>>> 4c462abff19aa3d8518a99e83af8d120230529e3
